8bb2e862cac7c2c085e70687e7dcf34f3c1f213d
极速蜗牛 <31986081+Jisu-Woniu@users.noreply.github.com>
2026-04-02 15:55:25 +0800

    Trigger hook.

--- bundle.ts
+++ bundle.ts
@@ -1,32 +1,27 @@
 // lightningcss --bundle -d dist ./src/index.css
-import { transform } from "lightningcss";
-import {
-  copyFile,
-  glob,
-  mkdir,
-  readFile,
-  writeFile,
-  constants,
-  rm,
-} from "node:fs/promises";
-import { basename } from "node:path";
+import { bundleAsync } from "lightningcss";
+import { copyFile, mkdir, writeFile, constants, rm } from "node:fs/promises";
+import { basename, dirname, relative, resolve } from "node:path";
 
-const { code } = transform({
+const { code, dependencies } = await bundleAsync({
   filename: "src/index.css",
-  code: await readFile("src/index.css"),
+  analyzeDependencies: { preserveImports: true },
+  minify: true,
 });
 
-const bundleFonts = async () => {
-  for await (const entry of glob("dist/*.woff2")) {
-    await rm(entry);
-  }
-  for await (const entry of glob("src/**/*.woff2")) {
-    const dest = `dist/${basename(entry)}`;
-    console.log(`${entry} -> ${dest}`);
-    await copyFile(entry, dest, constants.COPYFILE_EXCL);
-  }
-};
-
+await rm("dist", { recursive: true, force: true });
 await mkdir("dist", { recursive: true });
 
-await Promise.all([writeFile("dist/index.css", code), bundleFonts()]);
+const bundleFonts = (dependencies || [])
+  .filter((dependency) => dependency.type === "url")
+  .map((dependency) => {
+    const src = relative(
+      ".",
+      resolve(dirname(dependency.loc.filePath), dependency.url),
+    );
+    const dest = `dist/${dependency.placeholder}`;
+    console.log(`${src} -> ${dest}`);
+    return copyFile(src, dest, constants.COPYFILE_EXCL);
+  });
+
+await Promise.all([writeFile("dist/index.css", code), ...bundleFonts]);
